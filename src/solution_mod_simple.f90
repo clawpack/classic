16e442324cf4815ad23d9e3d7a6ba63cdfe2e7cc
Kyle T. Mandli <kyle.mandli@gmail.com>
2011-09-16 14:44:37 -0500

    Add simpler solution module prototype.

---
+++ src/solution_mod_simple.f90
@@ -0,0 +1,18 @@
+module solution_module
+
+    implicit none
+    
+
+contains
+
+end module solution_module
+
+program solution_test
+    
+    use solution_module
+
+    implicit none
+    
+
+
+end program solution_test
