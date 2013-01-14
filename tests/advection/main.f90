01dee4e5a502de07d82daadaf72f105f2fb5b913
Kyle Mandli <kyle.mandli@gmail.com>
2013-01-14 11:24:53 -0600

    Add real Riemann solver, update interfaces

--- tests/advection/main.f90
+++ tests/advection/main.f90
@@ -8,13 +8,13 @@ program advection1d
     type(solution_type) :: solution
     type(solver_type) :: solver
 
-    solution = new_solution(100,2,0,2)
+    solution = new_solution(100,2,0,2,0.d0,1.d0,0.d0)
 
     solution%q = 10.d2
 
     print "('q = ',2f9.1)", solution%q(1:2,12)
 
-    solver = new_solver()
+    solver = new_solver(2,solution)
 
     call step1(solution,solver)
  
