7f1d8ab0b59742668dec1a170c35c9ff1e133907
Kyle Mandli <kyle.mandli@gmail.com>
2025-05-21 23:41:02 -0500

    Convert 2d annulus test

--- examples/advection_2d_annulus/test_advection_2d_annulus.py
+++ examples/advection_2d_annulus/test_advection_2d_annulus.py
@@ -2,49 +2,27 @@
 Regression tests for 2D advection on an annulus.
 """
 
-from __future__ import absolute_import
 import sys
-import unittest
+import pytest
 
 import clawpack.classic.test as test
 
+def test_advection_2d_annulus(tmp_path, save):
 
-class Advection2DAnnulusTest(test.ClassicRegressionTest):
-    r"""Basic test for a 2D advection test case"""
+    ctr = test.ClawpackClassicTestRunner(tmp_path, __file__)
 
+    ctr.set_data()
+    ctr.rundata.clawdata.num_output_times = 2
+    ctr.rundata.clawdata.tfinal = 0.5
+    ctr.write_data()
 
-    def runTest(self, save=False):
+    ctr.executable_name = 'xclaw'
+    ctr.build_executable()
 
-        # Write out data files
-        self.load_rundata()
+    ctr.run_code()
 
-        self.rundata.clawdata.num_output_times = 2
-        self.rundata.clawdata.tfinal = 0.5
-
-        self.write_rundata_objects()
-
-        # Run code
-        self.run_code()
-
-        # Perform tests
-        self.check_frame(save=save, frame_num=1, 
-                         file_name='regression_data_test2.txt')
-        self.check_frame(save=save, frame_num=2, 
-                         file_name='regression_data_test3.txt')
-
-        self.success = True
-
-
+    ctr.check_frame(1, save=save)
+    ctr.check_frame(2, save=save)
 
 if __name__=="__main__":
-    if len(sys.argv) > 1:
-        if bool(sys.argv[1]):
-            # Fake the setup and save out output
-            test = Advection2DAnnulusTest()
-            try:
-                test.setUp()
-                test.runTest(save=True)
-            finally:
-                test.tearDown()
-            sys.exit(0)
-    unittest.main()
+    pytest.main([__file__])
